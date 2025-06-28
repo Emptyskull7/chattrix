7befeba202b22f55fccf60ca5ee784f0114dfad0
Akhilesh Pandey <pandeyakhilesh@Akhileshs-MacBook-Air.local>
2025-06-28 18:33:58 +0530

    Cors Setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
+import cors from 'cors';
+
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
@@ -12,6 +14,10 @@ dotenv.config();
 
 
 const PORT = process.env.PORT
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true, //allow frontend to send cookies
+}))
 
 app.use(express.json());
 app.use(cookieParser());
