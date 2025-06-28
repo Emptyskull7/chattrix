d4545aeded3dc67a3627ae393605eea280a6c4c0
Akhilesh Pandey <pandeyakhilesh@Akhileshs-MacBook-Air.local>
2025-06-28 17:42:38 +0530

    Setted-up TailwindCss,  Daisy-UI, React-Router, React-Toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+
+import {Route,Routes} from "react-router"
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import CallPage from './pages/CallPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import NotificationPage from "./pages/NotificationPage.jsx"
+
+import  {Toaster} from 'react-hot-toast'
+
 
 const App = () => {
   return (
-    <div className='bg-red-500 h-screen text-5xl'>App</div>
-  )
-}
+    <div className= "h-screen" data-theme="night">
+      <Routes>
+        <Route path="/"  element={<HomePage />} />
+        <Route path="/signup"  element={<SignUpPage />} />
+        <Route path="/login"  element={<LoginPage />} />
+        <Route path="/onboarding"  element={<OnboardingPage />} />
+        <Route path="/call"  element={<CallPage />} />
+        <Route path="/chat"  element={<ChatPage />} />
+        <Route path="/notification"  element={<NotificationPage />} />
+      </Routes>
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
